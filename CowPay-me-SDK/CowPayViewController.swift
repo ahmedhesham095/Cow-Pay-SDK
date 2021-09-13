80eb720c7100e8ebb6ca2ae6ea358f6d333f178b
Ahmed Hesham <alimido94@hotmail.com>
2021-09-13 23:25:14 +0200

    -- add ui

--- CowPay-me-SDK/CowPayViewController.swift
+++ CowPay-me-SDK/CowPayViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+enum CardType {
+    case credit
+    case fawry
+    case cashCollection
+}
+
 class CowPayViewController: UIViewController {
 
     @IBOutlet weak var creditCardView: UIView!
@@ -15,30 +21,134 @@ class CowPayViewController: UIViewController {
     @IBOutlet weak var cashCollectionTitle: UILabel!
     @IBOutlet weak var fawryTitle: UILabel!
     @IBOutlet weak var creditCardTitle: UILabel!
+    @IBOutlet weak var txtCardHolderName: UITextField!
+    @IBOutlet weak var txtCardNumber: UITextField!
+    @IBOutlet weak var txtExpiry: UITextField!
+    @IBOutlet weak var txtCVV: UITextField!
+    @IBOutlet weak var fawryLabel: UILabel!
+    @IBOutlet weak var creditCardStack: UIStackView!
+    @IBOutlet weak var cashCollectionStack: UIStackView!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtFloor: UITextField!
+    @IBOutlet weak var txtDistrict: UITextField!
+    @IBOutlet weak var txtApartment: UITextField!
+    @IBOutlet weak var lblGovernment: UILabel!
+    let expiryDatePicker = MonthYearPickerView()
+    var expiryDateToolBar: UIToolbar?
+    var selectedPaymentType: CardType?
+    var cardNumber: String?
+    var cardHolderName: String?
+    var CVV: String?
+    var expiryDateString: String?
+    var selectectedCity: String?
+    var cities = [   "Cairo",
+                     "Giza", "Haram",
+                     "Downtown Alex",
+                     "Sahel",
+                     "Behira",
+                     "Dakahlia",
+                     "El Kalioubia",
+                     "Gharbia",
+                     "Kafr Alsheikh",
+                     "Monufia",
+                     "Sharqia",
+                     "Isamilia",
+                     "Suez",
+                     "Port Said",
+                     "Damietta",
+                     "Fayoum",
+                     "Bani Suif",
+                     "Asyut",
+                     "Sohag",
+                     "Menya",
+                     "Qena",
+                     "Aswan",
+                     "Luxor"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.navigationItemTitle("Payment Method")
         self.navigationController?.navigationBar.barTintColor = UIColor(hexString: "44225A")
         setupCreditCard(self)
+        selectedPaymentType = .credit
+        txtCardHolderName.delegate = self
+        txtCardNumber.delegate = self
+        txtCVV.delegate = self
+        txtExpiry.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.expiryDateLabelTapped))
+        txtExpiry.isUserInteractionEnabled = true
+        txtExpiry.addGestureRecognizer(tapGesture)
     }
 
     @IBAction func setupCreditCard(_ sender: Any) {
         selectCreditCard()
         deselectFawry()
         deselectCashCollection()
+        creditCardStack.isHidden = false
+        fawryLabel.isHidden = true
+        cashCollectionStack.isHidden = true
+        selectedPaymentType = .credit
     }
     
     @IBAction func setupCashCollection(_ sender: Any) {
         selectCashCollection()
         deselectCreditCard()
         deselectFawry()
+        fawryLabel.isHidden = true
+        cashCollectionStack.isHidden = false
+        creditCardStack.isHidden = true
+        selectedPaymentType = .cashCollection
     }
     
     @IBAction func setupFawry(_ sender: Any) {
         selectFawry()
         deselectCreditCard()
         deselectCashCollection()
+        creditCardStack.isHidden = true
+        fawryLabel.isHidden = false
+        cashCollectionStack.isHidden = true
+        selectedPaymentType = .fawry
+    }
+    
+    @IBAction func selectGovernmentAction(_ sender: Any) {
+        let actionSheetController: UIAlertController = UIAlertController(title: "Select City", message: nil, preferredStyle: .actionSheet)
+        self.cities.forEach({ (city) in
+            let firstAction: UIAlertAction = UIAlertAction(title: city , style: .default) { action -> Void in
+                self.lblGovernment.text = city
+                self.selectectedCity = "\(self.cities.index(of: city))"
+            }
+            actionSheetController.addAction(firstAction)
+        })
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { action -> Void in
+        }
+        actionSheetController.addAction(cancelAction)
+        actionSheetController.popoverPresentationController?.sourceView = self.view
+        self.present(actionSheetController, animated: true , completion: nil)
+    }
+    
+    
+    @IBAction func confirmPayment(_ sender: Any) {
+        if selectedPaymentType == .credit {
+            if expiryDateString?.isEmpty == true {
+                AlertMessage(title: "", userMessage: "Please Choose Expiry Date")
+            } else {
+                //- Credit Card data
+                print("Bassiouny !!! \(cardNumber ?? "") \(expiryDateString ?? "") \(CVV ?? "") \(cardHolderName ?? "")")
+            }
+        }
+        
+        if selectedPaymentType == .fawry {
+            // navigate to fawry screen
+        }
+        
+        if selectedPaymentType == .cashCollection {
+            
+            if txtEmail.text?.isEmpty == false , txtAddress.text?.isEmpty == false , txtFloor.text?.isEmpty == false , txtDistrict.text?.isEmpty == false , txtApartment.text?.isEmpty == false , selectectedCity?.isEmpty == false {
+                print("Bassiouny !!! \(txtEmail.text ?? "")  \(txtAddress.text ?? "") \(txtFloor.text ?? "") \(txtDistrict.text ?? "")  \(txtApartment.text ?? "") \(selectectedCity ?? "")")
+            } else {
+              AlertMessage(title: "", userMessage: "Please Fill all the fields")
+            }
+        }
     }
     
     func selectCreditCard() {
@@ -70,8 +180,155 @@ class CowPayViewController: UIViewController {
         fawryView.borderColor = UIColor(hexString: "f6f6f6")
         fawryTitle.textColor =  UIColor.black
     }
+}
+//- Mark:// Validations
+extension CowPayViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == txtCardNumber {
+            if let txt = textField.text {
+                if txt.count >= 13 && txt.isStringContainsOnlyNumbers() && txt.luhnCheck() {
+                    cardNumber = textField.text
+                } else {
+                    AlertMessage(title: "", userMessage: "Invalid Card Number")
+                }
+            } else {
+                AlertMessage(title: "", userMessage: "Please Enter a card Number")
+            }
+        }
+        if textField == txtCardHolderName {
+            if textField.text?.isEmpty == true {
+                AlertMessage(title: "", userMessage: "Please Enter a card holder name")
+            } else {
+                cardHolderName = textField.text
+            }
+        }
+        
+        if textField == txtCVV {
+            if textField.text?.isEmpty == true {
+                AlertMessage(title: "", userMessage: "Please Enter an valid CVV")
+            } else {
+                CVV = textField.text
+            }
+        }
+        
+        if textField == txtEmail {
+            if textField.text?.isEmail == false {
+                AlertMessage(title: "", userMessage: "Please Enter an valid Email")
+            }
+        }
+    }
     
-   
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // validate credit card datta
+        if textField == txtCardNumber {
+            let maxLength = 16
+            let currentString: NSString = txtCardNumber.text! as NSString
+            let newString: NSString =
+                currentString.replacingCharacters(in: range, with: string) as NSString
+            return newString.length <= maxLength
+        } else if  textField == txtCVV {
+            let maxLength = 4
+            let currentString: NSString = textField.text! as NSString
+            let newString: NSString =
+                currentString.replacingCharacters(in: range, with: string) as NSString
+            return newString.length <= maxLength
+        } else if textField == txtCardHolderName {
+            let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+            let Regex = "[a-z A-Z ]+"
+            let predicate = NSPredicate.init(format: "SELF MATCHES %@", Regex)
+            if predicate.evaluate(with: text) || string == ""
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        }
+        return true
+    }
     
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == txtExpiry {
+            return false
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        DispatchQueue.main.async {
+            for case let view as UIToolbar in self.view.subviews {
+                view.removeFromSuperview()
+            }
+            for case let view as MonthYearPickerView in self.view.subviews {
+                view.removeFromSuperview()
+            }
+        }
+    }
     
 }
+
+
+extension CowPayViewController {
+    
+    @objc func expiryDateLabelTapped(sender:UITapGestureRecognizer) {
+        self.pickADate()
+    }
+    
+    private func pickADate() {
+        txtCardNumber.resignFirstResponder()
+        txtExpiry.resignFirstResponder()
+        txtCardHolderName.resignFirstResponder()
+        expiryDatePicker.backgroundColor = UIColor.white
+        expiryDatePicker.setValue(UIColor.black, forKey: "textColor")
+        expiryDatePicker.autoresizingMask = .flexibleWidth
+        expiryDatePicker.contentMode = .center
+        expiryDatePicker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(expiryDatePicker)
+        
+        expiryDateToolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
+        expiryDateToolBar?.barStyle = .default
+        expiryDateToolBar?.isTranslucent = true
+        let doneButton = UIBarButtonItem(title: "Done".localized(), style: UIBarButtonItem.Style.plain, target: self, action: #selector(didPressExpiryDone))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel".localized(), style: UIBarButtonItem.Style.plain, target: self, action: #selector(didPressExpiryCancel))
+        
+        expiryDateToolBar?.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        expiryDateToolBar?.isUserInteractionEnabled = true
+        DispatchQueue.main.async {
+            self.view.addSubview(self.expiryDateToolBar ?? UIToolbar())
+        }
+        expiryDatePicker.onDateSelected = { (month: Int, year: Int) in
+            let expiryString = String(format: "%02d/%02d", month , (year % 100) )
+            self.expiryDateString = expiryString
+        }
+    }
+        @objc func didPressExpiryDone() {
+            DispatchQueue.main.async {
+                for case let view as UIToolbar in self.view.subviews {
+                    view.removeFromSuperview()
+                }
+                for case let view as MonthYearPickerView in self.view.subviews {
+                    view.removeFromSuperview()
+                }
+            }
+            
+            if expiryDateString == nil  {
+                let year = NSCalendar(identifier: NSCalendar.Identifier.gregorian)!.component(.year, from: NSDate() as Date)
+                let month = NSCalendar(identifier: NSCalendar.Identifier.gregorian)!.component(.month, from: NSDate() as Date)
+                expiryDateString = String(format: "%02d/%02d", month , (year % 100) )
+            }
+            txtExpiry.text = expiryDateString
+        }
+    
+        @objc func didPressExpiryCancel() {
+            DispatchQueue.main.async {
+                for case let view as UIToolbar in self.view.subviews {
+                    view.removeFromSuperview()
+                }
+                for case let view as MonthYearPickerView in self.view.subviews {
+                    view.removeFromSuperview()
+                }
+            }
+        }
+    }
--- CowPay-me-SDK/Extensions/StringExtension.swift
+++ CowPay-me-SDK/Extensions/StringExtension.swift
@@ -124,6 +124,33 @@ extension String {
             return false
         }
     }
+    
+    func isStringContainsOnlyNumbers() -> Bool {
+          return self.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+      }
+    
+     func luhnCheck() -> Bool {
+        var sum = 0
+        let digitStrings = self.reversed().map { String($0) }
+        
+        for tuple in digitStrings.enumerated() {
+            if let digit = Int(tuple.element) {
+                let odd = tuple.offset % 2 == 1
+                
+                switch (odd, digit) {
+                case (true, 9):
+                    sum += 9
+                case (true, 0...8):
+                    sum += (digit * 2) % 9
+                default:
+                    sum += digit
+                }
+            } else {
+                return false
+            }
+        }
+        return sum % 10 == 0
+    }
 }
 
 extension String {
